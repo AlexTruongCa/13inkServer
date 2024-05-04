5107e8271dc0354bd6db28b0d37506044dcb1486
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-05-04 19:28:55 -0300

    add conditional check

--- functions/api.js
+++ functions/api.js
@@ -14,14 +14,28 @@ app.use(cors());
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const response = await fetch(url1, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
-    },
-  });
-  const apiResponseJson = await response.json();
-  res.send(apiResponseJson);
+  try {
+    const response = await fetch(url1, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from Instagram API");
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      const apiResponseJson = await response.json();
+      res.json(apiResponseJson);
+    } else {
+      throw new Error("Response is not in JSON format");
+    }
+  } catch (error) {
+    console.error("Error", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
   // console.log(apiResponseJson);
 });
 
