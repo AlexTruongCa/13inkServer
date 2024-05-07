c5002d41956c6fa6208b842a1e8f42016efab350
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-05-07 19:11:51 -0300

    update

--- functions/api.js
+++ functions/api.js
@@ -14,37 +14,39 @@ app.use(cors());
 const router = Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const response = await fetch(url1, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
-      },
-    });
-    console.log(response);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from Instagram API");
-    }
+  const response = await fetch(url1, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
+    },
+  });
+  const apiResponseJson = await response.json();
+  res.send(apiResponseJson);
 
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.includes("application/json")) {
-      let jsonData;
-      try {
-        jsonData = await response.json();
-      } catch (error) {
-        throw new Error("Failed to parse JSON response");
-      }
-      // const apiResponseJson = await response.json();
-      res.json(jsonData);
-    } else {
-      // const responseBody = await response.text();
-      // console.log("Response body:", responseBody);
-      throw new Error("Response is not in JSON format");
-    }
-  } catch (error) {
-    console.error("Error", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  // try {
+  //   const response = await fetch(url1, {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
+  //     },
+  //   });
+  //   if (!response.ok) {
+  //     throw new Error("Failed to fetch data from Instagram API");
+  //   }
+
+  //   const contentType = response.headers.get("content-type");
+  //   if (contentType && contentType.includes("application/json")) {
+  //     const apiResponseJson = await response.json();
+  //     res.json(apiResponseJson);
+  //   } else {
+  //     const responseBody = await response.text();
+  //     console.log("Response body:", responseBody);
+  //     throw new Error("Response is not in JSON format");
+  //   }
+  // } catch (error) {
+  //   console.error("Error", error);
+  //   res.status(500).json({ error: "Internal Server Error" });
+  // }
   // console.log(apiResponseJson);
 });
 
