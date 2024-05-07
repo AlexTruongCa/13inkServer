7ce19d765dc8619ef9aa5e3b84e477ac9f05b6b1
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-05-06 23:07:58 -0300

    updated

--- functions/api.js
+++ functions/api.js
@@ -1,38 +1,75 @@
+// import axios from "axios";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// import cors from "cors";
+// app.use(cors());
+
+// exports.handler = function instagram(event, context, callback) {
+//   const token = process.env.INSTAGRAM_TOKEN;
+//   const url = `https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink&access_token=${token}`;
+
+//   axios
+//     .get(url)
+//     .then(({ data: { data: posts } }) => {
+//       callback(null, {
+//         statusCode: 200,
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//         body: JSON.stringify(
+//           posts.map((i) => ({
+//             id: i.id,
+//             url: i.media_url,
+//             caption: i.caption,
+//             permalink: i.permalink,
+//             mediaType: i.media_type,
+//             username: i.username,
+//           }))
+//         ),
+//       });
+//     })
+//     .catch((e) => {
+//       callback(e);
+//     });
+// };
+
 import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
-import cors from "cors";
-app.use(cors());
 
-exports.handler = function instagram(event, context, callback) {
-  const token = process.env.INSTAGRAM_TOKEN;
-  const url = `https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink&access_token=${token}`;
+export async function handler(event, context) {
+  try {
+    const token = process.env.INSTAGRAM_TOKEN;
+    const url = `https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink&access_token=${token}`;
 
-  axios
-    .get(url)
-    .then(({ data: { data: posts } }) => {
-      callback(null, {
-        statusCode: 200,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(
-          posts.map((i) => ({
-            id: i.id,
-            url: i.media_url,
-            caption: i.caption,
-            permalink: i.permalink,
-            mediaType: i.media_type,
-            username: i.username,
-          }))
-        ),
-      });
-    })
-    .catch((e) => {
-      callback(e);
-    });
-};
+    const response = await axios.get(url);
+    const { data } = response.data;
+
+    const posts = data.map((post) => ({
+      id: post.id,
+      url: post.media_url,
+      caption: post.caption,
+      permalink: post.permalink,
+      mediaType: post.media_type,
+      username: post.username,
+    }));
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(posts),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
 
 // import dotenv from "dotenv";
 // import express, { Router } from "express";
