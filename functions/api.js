de28fd27a512c6efcf90fc8d409c51b997d99871
Alex Truong <145624358+AlexTruongCa@users.noreply.github.com>
2024-05-06 22:58:58 -0300

    update api.js with axios

--- functions/api.js
+++ functions/api.js
@@ -1,67 +1,51 @@
+import axios from "axios";
 import dotenv from "dotenv";
-import express, { Router } from "express";
-import serverless from "serverless-http";
-import cors from "cors";
 
 dotenv.config();
-
-const app = express();
-const url1 =
-  "https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink";
-
+import cors from "cors";
 app.use(cors());
 
-const router = Router();
+exports.handler = function instagram(event, context, callback) {
+  const token = process.env.INSTAGRAM_TOKEN;
+  const url = `https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink&access_token=${token}`;
 
-router.get("/", async (req, res) => {
-  try {
-    const response = await fetch(url1, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
-        "Content-Type": "application/json",
-      },
+  axios
+    .get(url)
+    .then(({ data: { data: posts } }) => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(
+          posts.map((i) => ({
+            id: i.id,
+            url: i.media_url,
+            caption: i.caption,
+            permalink: i.permalink,
+            mediaType: i.media_type,
+            username: i.username,
+          }))
+        ),
+      });
+    })
+    .catch((e) => {
+      callback(e);
     });
-    console.log(response);
-    if (!response.ok) {
-      throw new Error("Failed to fetch data from Instagram API");
-    }
-
-    const contentType = response.headers.get("content-type");
-    if (contentType && contentType.includes("application/json")) {
-      const apiResponseJson = await response.json();
-      const responseBody = JSON.stringify(apiResponseJson);
-      console.log("Response body:", responseBody);
-      res.json(responseBody);
-      res.json(apiResponseJson);
-    } else {
-      const responseBody = await response.text();
-      console.log("Response body:", responseBody);
-      throw new Error("Response is not in JSON format");
-    }
-  } catch (error) {
-    console.error("Error", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-  // console.log(apiResponseJson);
-});
-
-app.use("/", router);
-
-export const handler = serverless(app);
+};
 
 // import dotenv from "dotenv";
 // import express, { Router } from "express";
 // import serverless from "serverless-http";
-// import cors from "cors";
+// ;
 
-// dotenv.config();
+//
 
 // const app = express();
 // const url1 =
 //   "https://graph.instagram.com/me/media?fields=id,username,media_type,caption,media_url,permalink";
 
-// app.use(cors());
+// ;
 
 // const router = Router();
 
